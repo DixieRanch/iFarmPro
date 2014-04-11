c6398c65c820891f45a66840929a988c77b8c167
jersingh <jersingh@nmsu.edu>
2014-04-10 21:27:14 -0600

    removed bogus test with hardcoded path

--- spec/lib/tasks/import_spec.rb
+++ spec/lib/tasks/import_spec.rb
@@ -1,11 +1,12 @@
+require 'spec_helper'
+require 'rake'
+
 # references:
 # https://gist.github.com/equivalent/5107645
 # http://robots.thoughtbot.com/test-rake-tasks-like-a-boss
 # http://carlosplusplus.github.io/blog/2014/02/01/testing-rake-tasks-with-rspec/
 # http://pivotallabs.com/how-i-test-rake-tasks/
 # http://www.philsergi.com/2009/02/testing-rake-tasks-with-rspec.html
-
-require 'rake'
 
 describe 'app lib tasks import.rake' do
 
@@ -23,11 +24,6 @@ describe 'app lib tasks import.rake' do
   it 'validates weather URL' do
     agent.get(weather_url)
     expect(agent.page.uri.to_s).to eq 'http://weather.nmsu.edu/ws/data/etform/nmcc-da-1/'
-  end
-
-  it 'should find the proper rake file' do
-    file = Rails.root.join("#{task_path}.rake").to_s
-    expect(file).to eq '/home/joe/projects/daviet/ifarm/lib/tasks/import.rake'
   end
 
   it 'should execute test_task' do

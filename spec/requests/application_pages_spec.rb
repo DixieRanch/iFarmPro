b6f00cc8357349dc631d7b65ed8393e5dca88768
Greg Daviet <Greg@theDaviets.com>
2014-05-05 16:15:50 -0600

    Move Link tests to application_pages_spec  [skip ci]

--- spec/requests/application_pages_spec.rb
+++ spec/requests/application_pages_spec.rb
@@ -5,43 +5,85 @@ describe 'ApplicationPages' do
   subject { page }
 
   before do
-    sign_in(user)
     visit root_path
   end
 
   describe 'sidebar' do
     
-    it { should have_css '.sidebar-nav' }
-
     context 'when not signed in' do
-      before do
-        click_link 'Sign out'
-      end 
 
       it { should_not have_css '.sidebar-nav' }
     end
 
-    context 'when User has a farm' do
+    context 'when signed in' do
       before do
-        click_link 'Sign out'
         Company.current_id = user.company.id
         @farm = FactoryGirl.create(:farm)
         sign_in(user)
       end
       
       it { should have_css('.sidebar-nav', text: @farm.name) }
+
+      it 'should have the correct links' do
+        click_link 'Farms'
+        expect(page).to have_selector 'title', text: full_title('Farms')
+        click_link 'Irrigations'
+        expect(page).to have_selector 'title', text: full_title('Irrigations')
+        click_link 'Rain'
+        expect(page).to have_selector 'title', text: full_title('Rain')
+        click_link 'Irrigation'
+        expect(page).to have_selector 'title', text: full_title('Next Irrigation')
+      end
+    end
+  end
+  describe "layout links" do
+    
+    context "when signed out" do
+
+      it "should have the correct links" do
+        visit root_path
+        click_link "Help"
+        should have_selector 'title', text: full_title('Help')
+        click_link "Sign in"
+        should have_selector 'title', text: full_title('Sign in')
+        click_link "Home"
+        should have_selector 'title', text: full_title('')
+        click_link "About"
+        should have_selector 'title', text: full_title('About')
+        click_link "Contact"
+        should have_selector 'title', text: full_title('Contact')
+        click_link "iFarmPro"
+        should have_selector 'title', text: full_title('')
+        click_link "Sign up now!"
+        should have_selector 'title', text: full_title('Sign up')
+      end
     end
 
+    context "when signed in" do
 
-    it 'should have the correct links' do
-      click_link 'Farms'
-      expect(page).to have_selector 'title', text: full_title('Farms')
-      click_link 'Irrigations'
-      expect(page).to have_selector 'title', text: full_title('Irrigations')
-      click_link 'Rain'
-      expect(page).to have_selector 'title', text: full_title('Rain')
-      click_link 'Irrigation'
-      expect(page).to have_selector 'title', text: full_title('Next Irrigation')
+      it "should have the correct links" do
+        sign_in(user)
+        click_link "Help"
+        should have_selector 'title', text: full_title('Help')
+        # click_link "Farms"
+        # should have_selector 'title', text: full_title('Edit farms')
+        click_link "Company"
+        should have_selector 'title', text: full_title(user.company.name)
+        click_link "Edit User"
+        should have_selector 'title', text: full_title('Edit login')
+        click_link "Add User"
+        should have_selector 'title', text: full_title('Add User')
+        click_link "Home"
+        should have_selector 'title', text: full_title('')
+        click_link "About"
+        should have_selector 'title', text: full_title('About')
+        click_link "Contact"
+        should have_selector 'title', text: full_title('Contact')
+        click_link "iFarmPro"
+        should have_selector 'title', text: full_title(user.company.name)
+        click_link "Sign out"
+        should have_selector 'title', text: full_title('')
+      end
     end
   end
 end

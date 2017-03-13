5a628003f8c21b4e38eb42d9495a4a42122950f7
Greg Daviet <greg@dixie-ranch.com>
2017-03-13 20:13:43 +0000

    Fix formatted date test in rain_spec

--- spec/models/rain_spec.rb
+++ spec/models/rain_spec.rb
@@ -53,7 +53,8 @@ describe Rain do
 
     it 'formatted date' do
       rain.date = '4/1'
-      expect(rain.formatted_date).to eq 'April 1, 2016'
+      current_year = Time.new.year
+      expect(rain.formatted_date).to eq "April 1, #{current_year}"
     end
 
     it 'empty date' do

29589fdbbb3371e89c2a0d90b2c26629eff24d81
Greg Daviet <greg@dixie-ranch.com>
2017-08-12 13:15:58 +0000

    Update Model Annotations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,16 @@
 #
 # Table name: users
 #
-#  id              :integer          not null, primary key
-#  email           :string
-#  created_at      :datetime
-#  updated_at      :datetime
-#  password_digest :string
-#  remember_token  :string
-#  company_id      :integer
+#  id                :integer          not null, primary key
+#  email             :string
+#  created_at        :datetime
+#  updated_at        :datetime
+#  password_digest   :string
+#  remember_token    :string
+#  company_id        :integer
+#  activation_digest :string
+#  activated         :boolean          default(FALSE)
+#  activated_at      :datetime
 #
 
 class User < ActiveRecord::Base

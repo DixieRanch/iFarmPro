633ad1f940f2246261f7cc34ecf30ac0efa3b252
Michael Daviet <Michael@theDaviets.com>
2017-08-16 01:17:36 +0000

    Add Capybara-email gem for testing email

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/poltergeist'
 require 'shoulda/matchers'
+require 'capybara/email/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.

d8af19d48cd791f1310f99421140eada5925e870
Michael Daviet <michael@dixie-ranch.com>
2017-09-13 16:30:19 +0000

    Refactor password_resets_controller to have 5 line methods

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -13,16 +13,10 @@ class PasswordResetsController < ApplicationController
   def create
     email = params[:password_reset][:email]
     user = User.find_by(email: email)
-    if user
-      if user.activated?
-        user.send_password_reset_email
-        redirect_to password_reset_path(email)
-      else
-        redirect_to account_activation_path(email)
-      end
-    else
-      redirect_to password_reset_path(email)
-    end
+    @email = email
+    @user = user
+    
+    check_existance
   end
   
   def edit
@@ -32,25 +26,9 @@ class PasswordResetsController < ApplicationController
   def update
     @user = User.find_by(email: params[:user][:email])
     
-    if @user && @user.authenticated?(:password_reset, params[:id])
-      
-      if @user.password_reset_sent_at > 2.hours.ago
-        if @user.update(user_params)
-          sign_in(@user)
-          redirect_to root_path
-        else
-          render 'edit'
-        end
-      
-      else
-        flash[:danger] = "Expired Password Reset!  Request a new one."
-        redirect_to new_password_reset_path
-      end
-      
-    else
-      redirect_to password_reset_path(email: params[:user][:email])
-    end
+    check_authentication
   end
+  
   
   private
     
@@ -58,11 +36,61 @@ class PasswordResetsController < ApplicationController
       @user = User.find_by(email: params[:email])
     end
     
-    def check_expiration
-      if @user.password_reset_expired?
-        flash[:danger] = "Expired password reset!"
-        redirect_to new_password_reset_path
+    def check_existance
+      if @user
+        check_activation
+      else
+        redirect_to password_reset_path(@email)
       end
+    end
+    
+    def check_activation
+      if @user.activated?
+        send_email
+      else
+        redirect_to account_activation_path(@email)
+      end
+    end
+    
+    def send_email
+      @user.send_password_reset_email
+      redirect_to password_reset_path(@email)
+    end
+    
+    def check_authentication
+      if @user && @user.authenticated?(:password_reset, params[:id])
+        check_expiration
+        
+      else
+        redirect_to password_reset_path(email: params[:user][:email])
+      end
+    end
+    
+    def check_expiration
+      if @user.password_reset_sent_at > 2.hours.ago
+        check_validation
+      
+      else
+        is_expired
+      end
+    end
+    
+    def is_expired
+      flash[:danger] = "Expired Password Reset!  Request a new one."
+      redirect_to new_password_reset_path
+    end
+    
+    def check_validation
+      if @user.update(user_params)
+        login
+      else
+        render 'edit'
+      end
+    end
+    
+    def login
+      sign_in(@user)
+      redirect_to root_path
     end
     
     def user_params

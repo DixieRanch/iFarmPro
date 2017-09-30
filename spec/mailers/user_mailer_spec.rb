749db8c3dca369ee2323836d47548fced1ead5cd
Greg Daviet <greg@dixie-ranch.com>
2017-09-30 13:51:04 -0600

    Layout/AlignParameters: Align the parameters of a method call if they span more than one line (Rubocop) Layout/FirstParameterIndentation: Indent the first parameter one step more than the start of the previous line

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe UserMailer, type: :mailer do
     it "renders the body" do
       expect(mail).to have_selector 'h1', text: 'Welcome to iFarmPro!'
       expect(mail).to have_link 'Activate', href: edit_account_activation_url(
-                                                  user.activation_token, 
-                                                  email: user.email)
+        user.activation_token, 
+        email: user.email)
       expect(mail.body.encoded).to have_selector 'p', text: user.email
     end
   end
@@ -41,8 +41,8 @@ RSpec.describe UserMailer, type: :mailer do
       expect(mail).to have_selector 'p', text: 'reset your password'
       expect(mail.body.encoded).to have_selector 'p', text: user.email
       expect(mail).to have_link 'Reset Password', href: edit_password_reset_url(
-                                                        user.password_reset_token,
-                                                        email: user.email)
+        user.password_reset_token,
+        email: user.email)
     end
   end
 end

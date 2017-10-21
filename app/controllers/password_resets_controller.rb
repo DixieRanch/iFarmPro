dced16f00bfe27782b63c7c7a49d812cd816f49b
Greg Daviet <greg@dixie-ranch.com>
2017-10-21 08:24:52 -0600

    Refactor password_resets_controller#update

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -27,50 +27,23 @@ class PasswordResetsController < ApplicationController
 
   def update
     @user = User.find_by(email: params[:user][:email])
-
-    check_authentication(@user)
+    if @user.nil? || !@user.authenticated?(:password_reset, params[:id])
+      redirect_to password_reset_path(@user.email)
+    elsif @user.password_reset_sent_at < 2.hours.ago
+      flash[:danger] = 'Expired Password Reset!  Request a new one.'
+      redirect_to new_password_reset_path
+    elsif @user.update(user_params)
+      sign_in(@user)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def get_user
     @user = User.find_by(email: params[:email])
-  end
-
-  def check_authentication(user)
-    if user && user.authenticated?(:password_reset, params[:id])
-      check_expiration(user)
-
-    else
-      redirect_to password_reset_path(user.email)
-    end
-  end
-
-  def check_expiration(user)
-    if user.password_reset_sent_at > 2.hours.ago
-      check_validation(user)
-
-    else
-      is_expired
-    end
-  end
-
-  def is_expired
-    flash[:danger] = 'Expired Password Reset!  Request a new one.'
-    redirect_to new_password_reset_path
-  end
-
-  def check_validation(user)
-    if user.update(user_params)
-      login(user)
-    else
-      render 'edit'
-    end
-  end
-
-  def login(user)
-    sign_in(user)
-    redirect_to root_path
   end
 
   def user_params

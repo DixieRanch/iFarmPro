d338da97d9a5c366d400797d0254ee4bd0852945
Michael Daviet <michael@dixie-ranch.com>
2017-11-03 19:13:47 +0000

    Add test that email with no associated user gets redirected as though it had association.

--- spec/requests/password_reset_pages_spec.rb
+++ spec/requests/password_reset_pages_spec.rb
@@ -243,12 +243,18 @@ RSpec.describe 'PasswordReset', type: :request do
         expect(page).to have_title full_title('Request Password Reset')
       end
     end
-    
+
     context 'with email without associated user' do
       it 'renders the expired flash message' do
         visit edit_password_reset_path('token', email: 'Hacker@badguys.net')
-        
+
         expect(page).to have_css('div.alert.alert-danger', text: 'Expired')
+      end
+
+      it 'redirects to email request page' do
+        visit edit_password_reset_path('token', email: 'no_one@example.com')
+
+        expect(page).to have_title full_title('Request Password Reset')
       end
     end
   end

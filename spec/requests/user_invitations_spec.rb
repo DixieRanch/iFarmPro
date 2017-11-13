c5927fbe0a34ab8a0e721468b0271ec7eebada71
Michael Daviet <michael@dixie-ranch.com>
2017-11-13 19:11:31 +0000

    Add failing test for user invitation email

--- spec/requests/user_invitations_spec.rb
+++ spec/requests/user_invitations_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'UserInvitations' do
+<<<<<<< a860b4062addc0fb9c6ea4df7786bf6a22e1fe02
   context 'users dropdown' do
     it 'has invitation link' do
       user = create(:user)
@@ -18,10 +19,23 @@ describe 'UserInvitations' do
 
       fill_in 'Email', with: 'newUser@example.com'
 
+=======
+  
+  describe 'Email' do
+    
+    it 'sends invitation' do
+      user = create(:user)
+      sign_in user
+      visit new_user_path
+      
+      fill_in 'Email', with: 'newUser@example.com'
+      
+>>>>>>> Add failing test for user invitation email
       expect do
         click_button 'Save'
       end.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
+<<<<<<< a860b4062addc0fb9c6ea4df7786bf6a22e1fe02
 
     xit 'creates invitation object' do
       user = create(:user)
@@ -36,3 +50,7 @@ describe 'UserInvitations' do
     end
   end
 end
+=======
+  end
+end
+>>>>>>> Add failing test for user invitation email

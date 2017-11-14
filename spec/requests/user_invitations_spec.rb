ec14858db3fa98b035c060b2fc1c8351e2000f81
Michael Daviet <michael@dixie-ranch.com>
2017-11-14 00:00:54 +0000

    Add link in header for user invitation

--- spec/requests/user_invitations_spec.rb
+++ spec/requests/user_invitations_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 describe 'UserInvitations' do
+<<<<<<< ff5f42399d58afee8c1a7a40ef6868604f5687b4
 <<<<<<< a860b4062addc0fb9c6ea4df7786bf6a22e1fe02
+=======
+>>>>>>> Add link in header for user invitation
   context 'users dropdown' do
     it 'has invitation link' do
       user = create(:user)
@@ -10,6 +13,7 @@ describe 'UserInvitations' do
       expect(page).to have_link "Invite User"
     end
   end
+<<<<<<< ff5f42399d58afee8c1a7a40ef6868604f5687b4
   
   describe 'inviting new user' do
     xit 'sends invitation email' do
@@ -20,22 +24,30 @@ describe 'UserInvitations' do
       fill_in 'Email', with: 'newUser@example.com'
 
 =======
+=======
+>>>>>>> Add link in header for user invitation
   
-  describe 'Email' do
-    
-    it 'sends invitation' do
+  describe 'inviting new user' do
+    xit 'sends invitation email' do
       user = create(:user)
       sign_in user
       visit new_user_path
-      
+
       fill_in 'Email', with: 'newUser@example.com'
+<<<<<<< ff5f42399d58afee8c1a7a40ef6868604f5687b4
       
 >>>>>>> Add failing test for user invitation email
+=======
+
+>>>>>>> Add link in header for user invitation
       expect do
         click_button 'Save'
       end.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
+<<<<<<< ff5f42399d58afee8c1a7a40ef6868604f5687b4
 <<<<<<< a860b4062addc0fb9c6ea4df7786bf6a22e1fe02
+=======
+>>>>>>> Add link in header for user invitation
 
     xit 'creates invitation object' do
       user = create(:user)
@@ -48,9 +60,14 @@ describe 'UserInvitations' do
         click_button 'Save'
       end.to change(UserInvitation, :count).by(1)
     end
+<<<<<<< ff5f42399d58afee8c1a7a40ef6868604f5687b4
   end
 end
 =======
   end
 end
 >>>>>>> Add failing test for user invitation email
+=======
+  end
+end
+>>>>>>> Add link in header for user invitation

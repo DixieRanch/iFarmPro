2ac3e6ed45f283da2a0ef700bf30639ef399a1e2
Greg Daviet <greg@dixie-ranch.com>
2017-11-14 09:06:51 -0700

    Enable localhost HTTP requests for JS testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require 'capybara/poltergeist'
 require 'shoulda/matchers'
 require 'capybara/email/rspec'
 require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.

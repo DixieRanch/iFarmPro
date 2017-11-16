3f6f7c018947fbfc82cd82b6101862bf4408beda
Greg Daviet <greg@dixie-ranch.com>
2017-11-15 18:46:14 -0700

    Replace :let, :before, and :subject with explicit setup

--- spec/models/farm_spec.rb
+++ spec/models/farm_spec.rb
@@ -15,31 +15,24 @@ require 'rails_helper'
 describe Farm do
   valid_attributes = { name: 'Example Farm' }
 
-  let(:company)         { build_stubbed(:company) }
-  let(:weather_station) { create(:weather_station) }
-  let(:farm)            { weather_station.farms.new(valid_attributes) }
-
-  subject { farm }
-
-  before { Company.current_id = company.id }
-
-  it { should be_valid }
+  it { expect(Company.new(valid_attributes)).to be_valid }
 
   it 'should have a valid factory' do
-    farm = FactoryGirl.build(:farm)
-    expect(farm).to be_valid
+    set_tenant_company
+
+    expect(build_stubbed(:farm)).to be_valid
   end
 
   describe 'tenant security' do
-    it "should have only the current company's data" do
-      wrong_company = FactoryGirl.create(:company)
-      Company.current_id = wrong_company.id
-      child = weather_station.farms.create(valid_attributes)
-      expect(child).to be_valid
-      Company.current_id = company.id
-      farm.save
-      expect(Farm.all).not_to include(child)
-      expect(Farm.all).to include(farm)
+    it "has only the current company's data" do
+      set_tenant_company
+      other_companys_data = create :farm
+
+      set_tenant_company
+      this_companys_data = create :farm
+
+      expect(Farm.all).to include this_companys_data
+      expect(Farm.all).not_to include other_companys_data
     end
   end
 
@@ -50,11 +43,11 @@ describe Farm do
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:name) }
+    it { should validate_presence_of :name }
     it { should validate_uniqueness_of(:name).scoped_to(:company_id) }
     it { should validate_length_of(:name).is_at_most(50) }
-    it { should validate_presence_of(:company_id) }
-    it { should validate_presence_of(:weather_station) }
+    it { should validate_presence_of :company_id }
+    it { should validate_presence_of :weather_station }
   end
 
   describe 'associations' do
@@ -63,21 +56,21 @@ describe Farm do
     it { should belong_to :weather_station }
 
     it 'should return blocks ordered by name' do
-      farm.save
-      second = farm.blocks.create(name: 'Inbtween')
-      third = farm.blocks.create(name: 'Last')
-      first = farm.blocks.create(name: 'First')
-      correct_order = [first, second, third]
-      expect(farm.blocks.to_a).to eq correct_order
+      farm = create :farm
+      second = farm.blocks.create name: 'Inbtween'
+      third  = farm.blocks.create name: 'Last'
+      first  = farm.blocks.create name: 'First'
+
+      expect(farm.blocks.to_a).to eq [first, second, third]
     end
 
     it 'should return irrigation_wells ordered by name' do
-      farm.save
-      second = farm.irrigation_wells.create(name: 'Inbetween')
-      third = farm.irrigation_wells.create(name: 'Last')
-      first = farm.irrigation_wells.create(name: 'First')
-      correct_order = [first, second, third]
-      expect(farm.irrigation_wells.to_a).to eq correct_order
+      farm = create :farm
+      second = farm.irrigation_wells.create name: 'Inbetween'
+      third  = farm.irrigation_wells.create name: 'Last'
+      first  = farm.irrigation_wells.create name: 'First'
+
+      expect(farm.irrigation_wells.to_a).to eq [first, second, third]
     end
   end
 end

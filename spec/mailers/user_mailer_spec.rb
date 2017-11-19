7690ada027c94f945e8018433d2d6940cf6b3e97
Greg Daviet <greg@dixie-ranch.com>
2017-11-18 21:23:04 -0700

    Replace :let, :before, and :subject with explicit setup

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,49 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-  let(:user) { create(:user) }
-
   describe 'account_activation' do
-    let(:mail) { UserMailer.account_activation(user) }
-
     it 'renders the headers' do
-      expect(mail.subject).to eq('iFarmPro account activation')
-      expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(['noreply@ifarmpro.com'])
+      user = create :user
+
+      UserMailer.account_activation(user)
+
+      expect(last_email.subject).to eq('iFarmPro account activation')
+      expect(last_email.to).to eq([user.email])
+      expect(last_email.from).to eq(['noreply@ifarmpro.com'])
     end
 
     it 'renders the body' do
-      expect(mail).to have_selector 'h1', text: 'Welcome to iFarmPro!'
-      expect(mail).to have_link 'Activate', href: edit_account_activation_url(
-        user.activation_token,
-        email: user.email
-      )
-      expect(mail.body.encoded).to have_selector 'p', text: user.email
+      user = create :user
+
+      UserMailer.account_activation(user)
+
+      expect(last_email).to have_selector 'h1', text: 'Welcome to iFarmPro!'
+      expect(last_email).to have_link 'Activate',
+                                      href: edit_account_activation_url(
+                                        user.activation_token,
+                                        email: user.email
+                                      )
+      expect(last_email.body.encoded).to have_selector 'p', text: user.email
     end
   end
 
   describe 'password_reset' do
     it 'renders the headers' do
-      mail = UserMailer.password_reset(user)
+      user = create :user
 
       user.send_password_reset_email
 
-      expect(mail.subject).to eq('iFarmPro password reset')
-      expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(['noreply@ifarmpro.com'])
+      expect(last_email.subject).to eq('iFarmPro password reset')
+      expect(last_email.to).to eq([user.email])
+      expect(last_email.from).to eq(['noreply@ifarmpro.com'])
     end
 
-    it 'renders body' do
-      mail = UserMailer.password_reset(user)
+    it 'renders the body' do
+      user = create :user
 
       user.send_password_reset_email
 
-      expect(mail).to have_selector 'p', text: 'reset your password'
-      expect(mail.body.encoded).to have_selector 'p', text: user.email
-      expect(mail).to have_link 'Reset Password', href: edit_password_reset_url(
-        user.password_reset_token,
-        email: user.email
-      )
+      expect(last_email).to have_selector 'p', text: 'reset your password'
+      expect(last_email.body.encoded).to have_selector 'p', text: user.email
+      expect(last_email).to have_link 'Reset Password',
+                                      href: edit_password_reset_url(
+                                        user.password_reset_token,
+                                        email: user.email
+                                      )
     end
   end
 end

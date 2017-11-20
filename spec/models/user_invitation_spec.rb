9ad4dcfab9dcca3fa10b03d4e632fd85bbbc360e
Michael Daviet <michael@dixie-ranch.com>
2017-11-20 22:17:22 +0000

    Add email validations to UserInvitation

--- spec/models/user_invitation_spec.rb
+++ spec/models/user_invitation_spec.rb
@@ -1,11 +1,29 @@
 require 'rails_helper'
 
-describe UserInvitation do
+describe UserInvitation, :not_a_tenant_model do
   describe 'associations' do
     it 'respond to company' do
       invitation = UserInvitation.new
 
       expect(invitation).to respond_to :company
+    end
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+
+    it 'should validate email format' do
+      valid_email = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      invalid_email = %w[user@foo,com user_at_foo.org example.user@foo.
+                         foo@bar_baz.com foo@bar+baz.com]
+
+      valid_email.each do |email|
+        expect(UserInvitation.new).to allow_value(email).for(:email)
+      end
+
+      invalid_email.each do |email|
+        expect(UserInvitation.new).not_to allow_value(email).for(:email)
+      end
     end
   end
 end

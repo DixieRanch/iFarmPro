b767a8405cfcb8c925c4ebcda292ccb0fe70d1e9
Michael Daviet <michael@dixie-ranch.com>
2017-11-20 22:17:22 +0000

    Add email validations to UserInvitation

--- app/models/user_invitation.rb
+++ app/models/user_invitation.rb
@@ -1,3 +1,8 @@
 class UserInvitation < ActiveRecord::Base
   belongs_to :company
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX }
 end

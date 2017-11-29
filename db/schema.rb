c7e44beff58b5905c69750a244b01d2a3f7d4aff
Michael Daviet <michael@dixie-ranch.com>
2017-11-29 19:01:54 +0000

    Add token/digest for user_invitation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113230516) do
+ActiveRecord::Schema.define(version: 20171129184617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -202,6 +202,7 @@ ActiveRecord::Schema.define(version: 20171113230516) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "company_id"
+    t.string   "invitation_digest"
   end
 
   create_table "users", force: :cascade do |t|

514d7a64f4922a568459ce36ddb7b7e1327cfd75
Michael Daviet <michael@dixie-ranch.com>
2017-11-29 16:02:04 +0000

    resolve merge issues within user_invitations_spec

--- spec/requests/user_invitations_spec.rb
+++ spec/requests/user_invitations_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'UserInvitations' do
+<<<<<<< 84a0048ff6f4b662458f387269bf6cf48390ad1d
 <<<<<<< cfefa529eda5b5f0f7de192cdefd7b86cfdfe062
 <<<<<<< 07b7e09bea8eb72a4d182a839a3f7a8c1f7bc3f2
 <<<<<<< 1a769090a3bf0830502328c33ef42eeb83d8f2c0
@@ -145,10 +146,9 @@ end
 =======
 >>>>>>> Add link in header for user invitation
 >>>>>>> Add link in header for user invitation
-  context 'users dropdown' do
 =======
-  describe 'users dropdown' do
->>>>>>> Add form to submit email for invitations then redirect to schedule
+>>>>>>> resolve merge issues within user_invitations_spec
+  context 'users dropdown' do
     context 'invitation link' do
       it 'does exist' do
         user = create(:user)
@@ -167,6 +167,7 @@ end
       end
     end
   end
+<<<<<<< 84a0048ff6f4b662458f387269bf6cf48390ad1d
 <<<<<<< 6d268003e283b453e98cbad19a49773368bcb9b7
 <<<<<<< 07b7e09bea8eb72a4d182a839a3f7a8c1f7bc3f2
 >>>>>>> Add link in header for user invitation
@@ -178,50 +179,9 @@ end
 >>>>>>> Add link in header for user invitation
   
 =======
-
-<<<<<<< 4e8e6bbdd5235a0c0194a432d744a0c54af4ed9e
->>>>>>> Rubocop cleanup to user_invitations_spec
-  describe 'inviting new user' do
 =======
-  describe 'when inviting new user' do
-    it 'redirects to schedule' do
-      user = create(:user)
-      sign_in user
-      click_link 'Invite User'
-      fill_in 'Email', with: 'newUser@example.com'
+>>>>>>> resolve merge issues within user_invitations_spec
 
-      click_button 'Send Invitation'
-
-      expect(page).to have_title full_title 'Schedule'
-    end
-<<<<<<< 4178bfb756b00ea44e97a1dcdc655f187c02ea78
-    
->>>>>>> Add form to submit email for invitations then redirect to schedule
-=======
-
-    it 'displays a message' do
-      user = create(:user)
-      sign_in user
-      click_link 'Invite User'
-      fill_in 'Email', with: 'newUser@example.com'
-
-      click_button 'Send Invitation'
-
-      expect(page).to have_css('div.alert.alert-success', text: 'Invitation')
-    end
-
->>>>>>> Add flash message upon successful form submission
-    xit 'sends invitation email' do
-      user = create(:user)
-      sign_in user
-      visit new_user_path
-
-      fill_in 'Email', with: 'newUser@example.com'
-
-=======
-=======
->>>>>>> Add link in header for user invitation
-  
   describe 'inviting new user' do
     xit 'sends invitation email' do
       user = create(:user)
@@ -229,20 +189,11 @@ end
       visit new_user_path
 
       fill_in 'Email', with: 'newUser@example.com'
-<<<<<<< ff5f42399d58afee8c1a7a40ef6868604f5687b4
-      
->>>>>>> Add failing test for user invitation email
-=======
 
->>>>>>> Add link in header for user invitation
       expect do
         click_button 'Save'
       end.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
-<<<<<<< ff5f42399d58afee8c1a7a40ef6868604f5687b4
-<<<<<<< a860b4062addc0fb9c6ea4df7786bf6a22e1fe02
-=======
->>>>>>> Add link in header for user invitation
 
     xit 'creates invitation object' do
       user = create(:user)
@@ -255,6 +206,7 @@ end
         click_button 'Save'
       end.to change(UserInvitation, :count).by(1)
     end
+<<<<<<< 84a0048ff6f4b662458f387269bf6cf48390ad1d
 <<<<<<< ff5f42399d58afee8c1a7a40ef6868604f5687b4
   end
 end
@@ -276,3 +228,7 @@ end
 end
 >>>>>>> Add link in header for user invitation
 >>>>>>> Add link in header for user invitation
+=======
+  end
+end
+>>>>>>> resolve merge issues within user_invitations_spec

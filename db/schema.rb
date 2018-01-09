8283d1e8bd94185a21dc1c974c0c04fa7e23eaf9
Michael Daviet <michael@dixie-ranch.com>
2018-01-09 21:17:10 +0000

    Add invitation expiration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129184617) do
+ActiveRecord::Schema.define(version: 20180109204150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -203,6 +203,7 @@ ActiveRecord::Schema.define(version: 20171129184617) do
     t.datetime "updated_at"
     t.integer  "company_id"
     t.string   "invitation_digest"
+    t.datetime "invitation_sent_at"
   end
 
   create_table "users", force: :cascade do |t|

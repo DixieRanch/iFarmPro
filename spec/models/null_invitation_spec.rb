618a459b568732e7521842c4ad62a5289030e17b
Michael Daviet <michael@dixie-ranch.com>
2018-01-09 23:07:00 +0000

    Add null invitations to improve security for user invitation process.

---
+++ spec/models/null_invitation_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe NullInvitation, :not_a_tenant_model do
+  describe '#invitation_sent_at' do
+    it 'returns datetime midnight 1/1/1900' do
+      invitation = NullInvitation.new
+      
+      time = invitation.invitation_sent_at
+      
+      expect(time).to be < 7.days.ago
+    end
+  end
+end

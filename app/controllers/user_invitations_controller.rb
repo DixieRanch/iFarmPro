1fc06b32ff1dd2290011b5ee0c5cb93df54ca3c6
Michael Daviet <michael@dixie-ranch.com>
2018-01-11 21:30:21 +0000

    Add user creation functionality bounded by the company.

--- app/controllers/user_invitations_controller.rb
+++ app/controllers/user_invitations_controller.rb
@@ -1,5 +1,5 @@
 class UserInvitationsController < ApplicationController
-  skip_before_action :signed_in_user, only: [:edit]
+  skip_before_action :signed_in_user, only: [:edit, :update]
 
   def new
     @invitation = UserInvitation.new
@@ -27,12 +27,37 @@ class UserInvitationsController < ApplicationController
   def update
     @invitation = UserInvitation.with_email(params[:user_invitation][:email])
 
-    render 'edit'
+    if @invitation.invitation_expired?
+      redirect_to root_path
+
+    elsif params[:user_invitation][:password].present? # &&
+      # @invitation.update(invitation_params)
+      @user = User.new(user_params)
+      @user.company_id = @invitation.company_id
+      @user.save
+      @user.activate
+      sign_in(@user)
+
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def invitation_params
-    params.require(:user_invitation).permit([:email])
+    params.require(:user_invitation).permit(permitted_params)
+  end
+
+  def permitted_params
+    [:email, :company_id]
+  end
+
+  def user_params
+    params.require(:user_invitation).permit(user_permitted_params)
+  end
+
+  def user_permitted_params
+    [:email, :password, :password_confirmation, :company_id]
   end
 end

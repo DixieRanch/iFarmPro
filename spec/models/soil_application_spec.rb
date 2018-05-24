7e4c313a4bcc83c3e60ea5d49b093346e520435c
Michael Daviet <michael@dixie-ranch.com>
2018-05-24 21:38:38 +0000

    Rubocop corrections to soil_application_spec.

--- spec/models/soil_application_spec.rb
+++ spec/models/soil_application_spec.rb
@@ -69,19 +69,19 @@ describe SoilApplication do
 
       it 'has SoilApplication elements' do
         set_tenant_company
-        product = create(:soil_product, name: "Roundup")
+        product = create(:soil_product, name: 'Roundup')
         create(:soil_application, soil_product: product)
 
         expect(SoilApplication.next_applications.first)
           .to be_kind_of(SoilApplication)
       end
-      
+
       it 'only has elements that are herbicide applications' do
         set_tenant_company
-        product = create(:soil_product, name: "Roundup")
+        product = create(:soil_product, name: 'Roundup')
         herbicide = create(:soil_application, soil_product: product)
         fertilizer = create(:soil_application)
-        
+
         expect(SoilApplication.next_applications).not_to include(fertilizer)
       end
     end

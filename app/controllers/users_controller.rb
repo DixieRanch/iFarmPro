0cee1e998a8328e2938f60793b9b360dcd58abc4
Michael Daviet <michael@dixie-ranch.com>
2018-08-30 17:54:20 +0000

    Remove all access to 'Add Users' page, and asociated tests.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,6 @@
 #
 
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
   def create
     @user = User.new(user_params)
     @user.company_id = Company.current_id

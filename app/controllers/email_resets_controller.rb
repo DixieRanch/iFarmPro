3044b5f246c959291b89e982521fe294cbaf4b22
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2019-08-08 16:52:09 +0000

    Fixed rubocop offences

--- app/controllers/email_resets_controller.rb
+++ app/controllers/email_resets_controller.rb
@@ -1,14 +1,12 @@
 class EmailResetsController < ApplicationController
-  
   def show
     @email = params[:id]
   end
 
   def new
   end
-  
+
   def create
-    puts params
     redirect_to email_reset_path('#')
   end
 end

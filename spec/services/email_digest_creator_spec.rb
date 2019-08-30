74976cccfce0146d291e322f2fbd24ec798f388d
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2019-08-30 19:31:19 +0000

    Add test 'generates a digest for the given token'

--- spec/services/email_digest_creator_spec.rb
+++ spec/services/email_digest_creator_spec.rb
@@ -11,4 +11,14 @@ describe EmailDigestCreator, :not_a_tenant_model do
 
     expect(user.email_digest).not_to be_nil
   end
+
+  it 'generates a digest for the given token' do
+    user = User.find(create(:user).id)
+    token = SecureRandom.urlsafe_base64
+
+    EmailDigestCreator.new(user, token).call
+    digest = user.email_digest
+
+    expect(BCrypt::Password.new(digest).is_password?(token)).to be true
+  end
 end

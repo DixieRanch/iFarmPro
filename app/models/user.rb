90c6db33c67d600b3ff36314e8b9aa5a1df22854
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2019-09-03 17:36:18 +0000

    Remove activation_digest? accessor temp method

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,13 +78,6 @@ class User < ActiveRecord::Base
   # These will be removed to fully implement emial_digest
   # in following refactor steps.
 
-  def activation_digest?
-    ActiveSupport::Deprecation.warn(
-      'activation_digest? has been changed to email_digest?, use it instead'
-    )
-    email_digest?
-  end
-
   def password_reset_digest
     # ActiveSupport::Deprecation.warn(
     #   'password_reset_digest has been changed to email_digest, use it instead'

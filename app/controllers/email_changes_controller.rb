faea004a39676a177e99beb462ee8f7f3c58d4ae
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2019-09-13 18:57:49 +0000

    Add form controller changes

--- app/controllers/email_changes_controller.rb
+++ app/controllers/email_changes_controller.rb
@@ -9,19 +9,25 @@ class EmailChangesController < ApplicationController
   def create
     current_user.new_email = params[:email_changes][:new_email]
     if current_user.valid?
-      current_user.update_attributes(
-        new_email: params[:email_changes][:new_email]
-      )
-      render 'show'
+      valid_email
     else
-      current_user.update_attributes(
-        new_email: nil
-      )
-      flash[:danger] = 'Invalid Email Address'
-      render 'new'
+      invalid_email
     end
   end
 
-  def edit
+  private
+
+  def valid_email
+    current_user.update_attributes(
+      new_email: params[:email_changes][:new_email]
+    )
+    current_user.send_new_email_verification
+    render 'show'
+  end
+
+  def invalid_email
+    current_user.update_attributes(new_email: nil)
+    flash[:danger] = 'Invalid Email Address'
+    render 'new'
   end
 end

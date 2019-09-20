5104f28d84abe950dc4bbfcd9f40cfb077e003e9
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2019-09-20 13:41:42 +0000

    Refactor email_changes create

--- app/controllers/email_changes_controller.rb
+++ app/controllers/email_changes_controller.rb
@@ -9,7 +9,17 @@ class EmailChangesController < ApplicationController
 
   def create
     @user = current_user
-    @user.new_email = params[:user][:new_email]
+    assign_new_email_attribute_to(@user)
+    send_new_email_verification_or_display_form_errors
+  end
+
+  private
+
+  def assign_new_email_attribute_to(user)
+    user.new_email = params[:user][:new_email]
+  end
+
+  def send_new_email_verification_or_display_form_errors
     User.skip_callback(:save, :before, :create_remember_token)
     if @user.save
       @user.send_new_email_verification
@@ -19,21 +29,5 @@ class EmailChangesController < ApplicationController
       render 'email_changes/new'
     end
     User.set_callback(:save, :before, :create_remember_token)
-  end
-
-  private
-
-  def valid_email
-    current_user.update_attributes(
-      new_email: params[:email_changes][:new_email]
-    )
-    current_user.send_new_email_verification
-    render 'show'
-  end
-
-  def invalid_email
-    current_user.update_attributes(new_email: nil)
-    flash[:danger] = 'Invalid Email Address'
-    render 'new'
   end
 end

c5121af1b8e535d2e224e74a402d5682f05fb1a2
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2019-10-09 22:35:27 +0000

    Add token generation to TokenCreator

--- spec/services/token_creator_spec.rb
+++ spec/services/token_creator_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 describe TokenCreator, :not_a_tenant_model do
+  it 'creates a token' do
+    token = TokenCreator.call
+
+    expect(token).not_to eq nil
+  end
+
+  it 'creates a unique token' do
+    token1 = TokenCreator.call
+    token2 = TokenCreator.call
+
+    expect(token1).not_to eq token2
+  end
 end

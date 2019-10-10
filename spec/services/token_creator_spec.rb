b2bc86b195e241dc762f6b67550e627bb8baabf0
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2019-10-10 20:55:03 +0000

    Add token comparison to regex for a valid token

--- spec/services/token_creator_spec.rb
+++ spec/services/token_creator_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe TokenCreator, :not_a_tenant_model do
   it 'creates a token' do
     token = TokenCreator.call
+    token_regex = /\S{22}/
 
-    expect(token).not_to eq nil
+    expect(token).not_to eq token_regex
   end
 
   it 'creates a unique token' do

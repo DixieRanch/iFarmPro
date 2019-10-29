de727a71831919b88182b5cbe19b88056cd3def4
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2019-10-29 22:41:00 +0000

    Remove white space

--- spec/requests/boxes_spec.rb
+++ spec/requests/boxes_spec.rb
@@ -171,7 +171,7 @@ RSpec.describe 'Boxes' do
         expect(page).to_not have_selector 'td', text: '001'
         expect(page).to_not have_selector 'td', text: '200'
       end
-      
+
       it 'flashes success message' do
         sign_in create(:user)
         create(:box, name: '001', empty_weight: 200)
@@ -181,7 +181,7 @@ RSpec.describe 'Boxes' do
         fill_in 'Name', with: '2019-002'
         fill_in 'Empty Weight', with: '210'
         click_button 'Save Container'
-        
+
         expect(page).to have_css '.alert-success'
       end
     end

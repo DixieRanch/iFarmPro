970ce1adef7408fae353a28078b7373b09efa9be
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2020-02-06 18:20:21 +0000

    Add sidebar link for storage management

--- spec/requests/storage_management_spec.rb
+++ spec/requests/storage_management_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'StorageManagement', type: :request do
+  describe 'Storage Management link' do
+    context 'when clicked' do
+      it 'redirects to stroage management page' do
+        sign_in(create(:user))
+
+        click_link 'Storage Management'
+
+        expect(page).to have_title full_title 'Storage Management'
+      end
+    end
+  end
 end

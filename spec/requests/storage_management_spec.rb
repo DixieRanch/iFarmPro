c3f117b6c152e0bfde6e9efd94fa05bb03b8579c
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2020-02-06 18:42:49 +0000

    Add table title to index page

--- spec/requests/storage_management_spec.rb
+++ spec/requests/storage_management_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe 'StorageManagement', type: :request do
       end
     end
   end
+
+  describe 'location index' do
+    it 'has correct title' do
+      sign_in(create(:user))
+      visit storage_management_index_path
+
+      expect(page).to have_selector 'h1', text: 'Location Overview'
+    end
+  end
 end

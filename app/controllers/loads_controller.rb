5e9215ac46f9b3d7b4b33012f88430387c440d66
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2020-02-21 20:59:23 +0000

    Change to use @loads instead of @locations

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -1,7 +1,8 @@
+require 'will_paginate/array'
 class LoadsController < ApplicationController
   def index
-    @loads = Load.all
-    @locations = locations_list
+    @loads = Load.all.paginate(page: params[:page], per_page: 30)
+    # @locations = locations_list
     @total_net_weight = total_net_weight
     @total_lot_count = total_lot_count
   end

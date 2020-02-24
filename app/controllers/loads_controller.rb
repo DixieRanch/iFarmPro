f9e3d4aca10797ac8cc5c06affedd77075d4d35c
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2020-02-24 15:17:06 +0000

    Remove unused lines

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -3,8 +3,6 @@ class LoadsController < ApplicationController
   def index
     @loads = Load.all.paginate(page: params[:page], per_page: 30)
     @totals = totals(@loads)
-    # @total_net_weight = total_net_weight(@loads)
-    # @total_lot_count = total_lot_count(@loads)
   end
 
   def edit
@@ -16,22 +14,6 @@ class LoadsController < ApplicationController
 
   def locations_list
     FreezerLocation.page(params[:page]).by_name_numerically
-  end
-
-  def total_net_weight(loads)
-    weight = 0
-    loads.each do |ld|
-      weight += ld.lots.all.map(&:net_weight).sum
-    end
-    weight
-  end
-
-  def total_lot_count(loads)
-    count = 0
-    loads.each do |ld|
-      count += ld.lots.all.count
-    end
-    count
   end
 
   def totals(loads)

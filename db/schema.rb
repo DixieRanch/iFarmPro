61ec3052e3601db36da1a1af80404f2865d9af98
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2020-02-26 23:01:28 +0000

    Add contents to lot database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190919151205) do
+ActiveRecord::Schema.define(version: 20200226225208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -160,6 +160,7 @@ ActiveRecord::Schema.define(version: 20190919151205) do
     t.integer  "field_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "contents"
   end
 
   create_table "meter_readings", force: :cascade do |t|

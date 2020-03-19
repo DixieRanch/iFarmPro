75eae507ef52c5d1594b8887e132185fd66b50af
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2020-03-19 22:58:11 +0000

    Add spec for irrigation intervals that crosses a new year

--- spec/services/next_irrigation_spec.rb
+++ spec/services/next_irrigation_spec.rb
@@ -7,4 +7,10 @@ describe NextIrrigation, :not_a_tenant_model do
     expect(NextIrrigation.new(irrigation, irrigation.field).next_date).to be_kind_of(Date)
     expect(NextIrrigation.new(irrigation, irrigation.field).next_date).to be > irrigation.time.to_date
   end
+
+  it 'should properly handle irrigation interval that crosses new year' do
+    irrigation = build_stubbed :irrigation, time: 'Dec 30, 2012 12:00'
+
+    expect(NextIrrigation.new(irrigation, irrigation.field).next_date.year).to be 2013
+  end
 end

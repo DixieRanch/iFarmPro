d2dfc2e8b1a344bb20fbfdd172daa252068854f4
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2020-03-19 22:59:48 +0000

    Add specs for after a rain event

--- spec/services/next_irrigation_spec.rb
+++ spec/services/next_irrigation_spec.rb
@@ -13,4 +13,32 @@ describe NextIrrigation, :not_a_tenant_model do
 
     expect(NextIrrigation.new(irrigation, irrigation.field).next_date.year).to be 2013
   end
+
+  context 'after rain' do
+    it 'has later date' do
+      irrigation = build_stubbed(:irrigation)
+      farm = irrigation.field.block.farm
+      pre_rain_irrigation_date = irrigation.next_irrigation_date
+
+      create(:rain, date: pre_rain_irrigation_date - 1, farm: farm)
+      post_rain_irrigation_date = irrigation.next_irrigation_date
+
+      expect(post_rain_irrigation_date).to be > pre_rain_irrigation_date
+    end
+
+    it 'same date if available water is at maximum' do
+      irrigation = build_stubbed :irrigation
+      create(:rain, date:   irrigation.time.to_date + 2,
+                    amount: 10.0,
+                    farm:   irrigation.field.block.farm)
+      current_date = irrigation.next_irrigation_date
+      create(:rain, date:   irrigation.time.to_date + 1,
+                    amount: 10.0,
+                    farm:   irrigation.field.block.farm)
+
+      future_date = irrigation.next_irrigation_date
+
+      expect(future_date).to eq current_date
+    end
+  end
 end

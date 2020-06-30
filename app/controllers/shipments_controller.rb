07c688fe5dabd789cc2aecb21aea1c200ff3181e
Elizabeth Van Dyke <Elizabeth@DixieRanch.com>
2020-06-30 17:58:03 +0000

    Create shipments link

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,4 +1,4 @@
 class ShipmentsController < ApplicationController
-    def index
-    end
+  def index
+  end
 end
--- spec/requests/shipments_spec.rb
+++ spec/requests/shipments_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Shipments', type: :request do
   describe 'shipments page' do
     it 'has correct title' do
-        sign_in create(:user)
-        visit shipments_path
-        
-        expect(page).to have_selector 'h1', text: 'Shipment Overview'
+      sign_in create(:user)
+      visit shipments_path
+
+      expect(page).to have_selector 'h1', text: 'Shipment Overview'
     end
   end
 end

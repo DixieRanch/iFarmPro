88a6587bb5487641877056992db75a82ebaf0547
Elizabeth Van Dyke <Elizabeth@DixieRanch.com>
2020-08-04 17:35:58 +0000

    Remove unneeded location create from displays the box name for the lot

--- spec/requests/shipping_spec.rb
+++ spec/requests/shipping_spec.rb
@@ -31,8 +31,7 @@ RSpec.describe 'Shipping', type: :request do
     it 'displays the box name for the lot' do
       sign_in create(:user)
       shipment = create(:shipment)
-      location = create(:freezer_location)
-      lot = create(:lot, freezer_location: location)
+      lot = create(:lot)
       visit loads_path
       click_link 'Ship Location'
       select(shipment.name, from: 'Shipment')

0009b1a5428683f5de1cf6b4ee9e27d2b0b34506
Elizabeth Van Dyke <Elizabeth@DixieRanch.com>
2020-08-04 17:38:19 +0000

    Remove unneeded location create from updates the shipment_id of the lot

--- spec/requests/shipping_spec.rb
+++ spec/requests/shipping_spec.rb
@@ -57,8 +57,7 @@ RSpec.describe 'Shipping', type: :request do
     it 'updates the shipment_id of the lot' do
       sign_in create(:user)
       shipment = create(:shipment)
-      location = create(:freezer_location)
-      lot = create(:lot, freezer_location: location)
+      lot = create(:lot)
       visit loads_path
       click_link 'Ship Location'
       select(shipment.name, from: 'Shipment')

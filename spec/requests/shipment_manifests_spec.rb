6b87488d273b9b5b0cb724b69a9369d2fe6f64b8
Elizabeth Van Dyke <Elizabeth@DixieRanch.com>
2020-08-12 21:36:10 +0000

    Add display of lots to shipment inspect page

--- spec/requests/shipment_manifests_spec.rb
+++ spec/requests/shipment_manifests_spec.rb
@@ -10,5 +10,16 @@ RSpec.describe 'ShipmentManifests', type: :request do
       expect(page).to have_selector 'h1', text:
                                            shipment.name.to_s + ' Manifest'
     end
+
+    it 'displays the names of shipped lots' do
+      sign_in create(:user)
+      shipment = create(:shipment)
+      lot1 = create(:lot, shipment_id: shipment.id, freezer_location_id: nil)
+      lot2 = create(:lot, shipment_id: shipment.id, freezer_location_id: nil)
+      visit shipment_manifest_path(shipment.id)
+
+      expect(page).to have_selector 'td', text: lot1.name
+      expect(page).to have_selector 'td', text: lot2.name
+    end
   end
 end
